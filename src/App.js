52996f3f0c70da7106f83dc821e3501f55a7c16f
gurumurthi97 <guru13041997@gmail.com>
2022-10-21 21:46:01 +0530

    this is updated

--- src/App.js
+++ src/App.js
@@ -3,19 +3,61 @@ import Header from "./components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import Search from "./pages/Search";
 import CoinPage from "./pages/CoinPage";
+import ComparePage from "./pages/Compare";
+import { useEffect } from "react";
 
 function App() {
+  // var cursor;
+  // var cursorPointer;
+
+  // useEffect(() => {
+  //   cursor = document.getElementById("cursor");
+  //   document.body.addEventListener("mousemove", function (e) {
+  //     return (
+  //       (cursor.style.left = e.clientX + "px"),
+  //       (cursor.style.top = e.clientY + "px")
+  //     );
+  //   });
+
+  //   cursorPointer = document.getElementById("cursor-pointer");
+  //   document.body.addEventListener("mousemove", function (e) {
+  //     return (
+  //       (cursorPointer.style.left = e.clientX + "px"),
+  //       (cursorPointer.style.top = e.clientY + "px")
+  //     );
+  //   });
+  //   document.body.addEventListener("mousedown", function (e) {
+  //     return (
+  //       (cursor.style.height = "0.5rem"),
+  //       (cursor.style.width = "0.5rem"),
+  //       (cursorPointer.style.height = "2.5rem"),
+  //       (cursorPointer.style.width = "2.5rem")
+  //     );
+  //   });
+  //   document.body.addEventListener("mouseup", function (e) {
+  //     return (
+  //       (cursor.style.height = "0.3rem"),
+  //       (cursor.style.width = "0.3rem"),
+  //       (cursorPointer.style.height = "2rem"),
+  //       (cursorPointer.style.width = "2rem")
+  //     );
+  //   });
+  // }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/coin" element={<CoinPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div className="cursor" id="cursor"></div>
+      <div className="cursor-pointer" id="cursor-pointer"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/compare" element={<ComparePage />} />
+          <Route path="/coin" element={<CoinPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
